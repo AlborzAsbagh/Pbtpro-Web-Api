4ba1275fdf7b565163b301dd67714d2c4a253fa3
Alborz <alborzasbagh@yahoo.com>
2024-01-26 12:49:43 +0300

    Personel Fields Name Added

--- WebApiNew/Models/Personel.cs
+++ WebApiNew/Models/Personel.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace WebApiNew.Models
 {
@@ -7,9 +8,25 @@ namespace WebApiNew.Models
     {
         [DataMember] public int TB_PERSONEL_ID { get; set; }
 
+        [DataMember] public int PRS_OLUSTURAN_ID { get; set; }
+
+        [DataMember] public int PRS_DEGISTIREN_ID { get; set; }
+
         [DataMember] public int PRS_RESIM_ID { get; set; }
 
-        [DataMember] public string PRS_PERSONEL_KOD { get; set; }
+        [DataMember] public string PRS_ADRES { get; set; }
+
+        [DataMember] public string PRS_IL { get; set; }
+
+        [DataMember] public string PRS_ILCE { get; set; }
+
+        [DataMember] public string PRS_ACIKLAMA { get; set; }
+
+        [DataMember] public string PRS_ULKE { get; set; }
+
+        [DataMember] public string PRS_POSTA_KOD { get; set; }
+
+		[DataMember] public string PRS_PERSONEL_KOD { get; set; }
 
         [DataMember] public string PRS_ISIM { get; set; }
 
@@ -43,6 +60,10 @@ namespace WebApiNew.Models
 
         [DataMember] public int PRS_GOREV_KOD_ID { get; set; }
 
+        [DataMember] public int PRS_FIRMA_ID { get; set; }
+
+        [DataMember] public string PRS_FIRMA { get; set; }
+
 	    [DataMember] public string PRS_GOREV { get; set; }
 
         [DataMember] public int PRS_LOKASYON_ID { get; set; }
@@ -56,6 +77,10 @@ namespace WebApiNew.Models
         [DataMember] public string PRS_MASRAF_MERKEZI { get; set; }
 
         [DataMember] public string PRS_TELEFON { get; set; }
+
+        [DataMember] public string PRS_TELEFON1 { get; set; }
+
+        [DataMember] public string PRS_FAX { get; set; }
 
         [DataMember] public string PRS_DAHILI { get; set; }
 
@@ -89,6 +114,101 @@ namespace WebApiNew.Models
 
         [DataMember] public int PRS_IDARI_PERSONEL_ID { get; set; }
 
-		[DataMember] public string PRS_IDARI_PERSONEL { get; set; }
+		[DataMember] public string PRS_IDARI_PERSONEL_YAZI { get; set; }
+
+		[DataMember] public string PRS_CINSIYET { get; set; }
+
+		[DataMember] public string PRS_KAN_GRUP { get; set; }
+
+		[DataMember] public string PRS_SSK_NO { get; set; }
+
+		[DataMember] public string PRS_VERGI_NO { get; set; }
+
+		[DataMember] public string PRS_EGITIM_DURUMU { get; set; }
+
+		[DataMember] public string PRS_MEZUN_OLDUGU_OKUL { get; set; }
+
+		[DataMember] public string PRS_MEZUN_OLDUGU_BOLUM { get; set; }
+
+        [DataMember] public int PRS_DIL_KOD_ID { get; set; }
+
+		[DataMember] public string PRS_DIL { get; set; }
+
+		[DataMember] public int PRS_UYRUK_KOD_ID { get; set; }
+
+		[DataMember] public string PRS_UYRUK { get; set; }
+
+		[DataMember] public int PRS_UCRET_TIPI { get; set; }
+
+        [DataMember] public string PRS_UCRET_TIPI_YAZI { get; set; }
+
+        [DataMember] public DateTime? PRS_MEZUNIYET_TARIH { get; set; }
+
+        [DataMember] public DateTime? PRS_ISE_BASLAMA { get; set; }
+
+        [DataMember] public DateTime? PRS_AYRILMATARIH { get; set; }
+
+        [DataMember] public DateTime? PRS_OLUSTURMA_TARIH { get; set; }
+
+        [DataMember] public DateTime? PRS_DEGISTIRME_TARIH { get; set; }
+
+		[DataMember] public DateTime? PRS_DOGUM_TARIH { get; set; }
+
+		[DataMember] public DateTime? PRS_KIMLIK_VERILIS_TARIH { get; set; }
+
+		[DataMember] public DateTime? PRS_EHLIYET_BELGE_TARIHI { get; set; }
+
+		[DataMember] public float PRS_FAZLA_MESAI { get; set; }
+
+        [DataMember] public float PRS_BIRIM_UCRET { get; set; }
+
+        [DataMember] public string PRS_TCKIMLIK_NO { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_SERINO { get; set; }
+
+        [DataMember] public string PRS_BABA_ADI { get; set; }
+
+        [DataMember] public string PRS_ANNE_ADI { get; set; }
+
+        [DataMember] public string PRS_DOGUM_YERI { get; set; }
+
+        [DataMember] public string PRS_DINI { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_KAYIT_NO { get; set; }
+
+        [DataMember] public string PRS_KAYITLI_OLDUGU_IL { get; set; }
+
+        [DataMember] public string PRS_KAYITLI_OLDUGU_ILCE { get; set; }
+
+        [DataMember] public string PRS_MEDENI_HALI { get; set; }
+
+        [DataMember] public string PRS_MAHALLE_KOY { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_CILT_NO { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_AILE_SIRA_NO { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_SIRA_NO { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_VERILDIGI_YER { get; set; }
+
+        [DataMember] public string PRS_KIMLIK_VERILIS_NEDENI { get; set; }
+
+        [DataMember] public string PRS_EHLIYET { get; set; }
+
+        [DataMember] public string PRS_EHLIYET_SINIF { get; set; }
+
+        [DataMember] public string PRS_EHLIYET_VERILDIGI_IL_ILCE { get; set; }
+
+        [DataMember] public string PRS_EHLIYET_SERI_NO { get; set; }
+
+        [DataMember] public float PRS_CEZAPUAN { get; set; }
+
+        [DataMember] public string PRS_EHLIYET_KULLANDIGI_CIHAZ_PROTEZ { get; set; }
+
+        [DataMember] public string PRS_EHLIYETNO { get; set; }
+
+
+
 	}
 }
