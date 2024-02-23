ecf926cf102322438e1a33b1210ba9ed818087ad
Alborz <alborzasbagh@yahoo.com>
2024-02-23 13:58:13 +0300

    Web Dashboard Items Added Separately

--- WebApiNew/Controllers/WebDashboardController.cs
+++ WebApiNew/Controllers/WebDashboardController.cs
@@ -1,13 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Web.Http;
 using Dapper;
-using Newtonsoft.Json.Linq;
 using WebApiNew.Filters;
-using WebApiNew.Models;
 
 namespace WebApiNew.Controllers
 {
@@ -19,42 +15,187 @@ namespace WebApiNew.Controllers
 		List<Prm> parametreler = new List<Prm>();
 		Parametreler prms = new Parametreler();
 
-		//Get Lokasyon List For Web App ( Lokasyon Sayfasi Icin )
-		[Route("api/GetDashboardItems")]
+		[Route("api/GetDashboardCards")]
 		[HttpGet]
-		public WebDashboard GetDashboardItems([FromUri] int ID)
+		public object GetDashboardCards([FromUri] int ID)
 		{
-			prms.Clear();
-			prms.Add("ISM_ID", ID);
-			WebDashboard entity = new WebDashboard();
-			query = @"
+			try
+			{
+				prms.Clear();
+				prms.Add("ISM_ID", ID);
+				WebDashboardCards entity = new WebDashboardCards();
+				query = @"
 						  SELECT
 						  (SELECT count(*) FROM orjin.TB_ISEMRI WHERE ISM_KAPATILDI = 0) AS ACIK_IS_EMIRLERI,
 						  (SELECT count(*) FROM orjin.TB_IS_TALEBI WHERE IST_DURUM_ID != 4 AND IST_DURUM_ID != 5) AS DEVAM_EDEN_IS_TALEPLERI,
-						  (SELECT count(*) FROM orjin.TB_STOK stk where stk.STK_MIKTAR < stk.STK_MIN_MIKTAR) AS DUSUK_STOKLU_MALZEMELER  ";
+						  (SELECT count(*) FROM orjin.TB_STOK stk where stk.STK_MIKTAR < stk.STK_MIN_MIKTAR) AS DUSUK_STOKLU_MALZEMELER  ,
+						  (SELECT count(*) FROM orjin.TB_MAKINE where MKN_AKTIF = 1  ) AS MAKINE_SAYISI";
 
-			DataTable dt = klas.GetDataTable(query,prms.PARAMS);
-			entity.ACIK_IS_EMIRLERI = Convert.ToInt32(dt.Rows[0]["ACIK_IS_EMIRLERI"]);
-			entity.DEVAM_EDEN_IS_TALEPLERI = Convert.ToInt32(dt.Rows[0]["DEVAM_EDEN_IS_TALEPLERI"]);
-			entity.DUSUK_STOKLU_MALZEMELER = Convert.ToInt32(dt.Rows[0]["DUSUK_STOKLU_MALZEMELER"]);
-
-			query = " SELECT * FROM orjin.UDF_TIPE_GORE_MAKINE_SAYISI(32501) ";
-			dt = klas.GetDataTable(query, prms.PARAMS);
-
-			for(int i = 0; i<dt.Rows.Count; i++)
-			{
-				entity.MAKINE_TIP_ENVANTER.Add(new MakineTipEnvanteri
-					(
-
-					Convert.ToInt32(dt.Rows[i]["TB_KOD_ID"]),
-					Convert.ToString(dt.Rows[i]["MAKINE_TIPI"]),
-					Convert.ToInt32(dt.Rows[i]["MAKINE_SAYISI"])
-					
-					));
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+				entity.ACIK_IS_EMIRLERI = Convert.ToInt32(dt.Rows[0]["ACIK_IS_EMIRLERI"]);
+				entity.DEVAM_EDEN_IS_TALEPLERI = Convert.ToInt32(dt.Rows[0]["DEVAM_EDEN_IS_TALEPLERI"]);
+				entity.DUSUK_STOKLU_MALZEMELER = Convert.ToInt32(dt.Rows[0]["DUSUK_STOKLU_MALZEMELER"]);
+				entity.MAKINE_SAYISI = Convert.ToInt32(dt.Rows[0]["MAKINE_SAYISI"]);
+				return entity;
 			}
-
-			return entity;
+			catch(Exception ex) 
+			{
+				return Json(new { error = ex.Message });
+			}
 		}
 
+		[Route("api/GetMakineTipEnvanter")]
+		[HttpGet]
+		public object GetMakineTipEnvanter([FromUri] int ID)
+		{
+			List<MakineTipEnvanteri> makineTipList = new List<MakineTipEnvanteri>();
+			prms.Clear();
+			prms.Add("ISM_ID", ID);
+			try
+			{
+				query = " SELECT * FROM orjin.WEB_DASH_UDF_TIPE_GORE_MAKINE_SAYISI() ";
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					makineTipList.Add(new MakineTipEnvanteri
+						(
+
+						Convert.ToInt32(dt.Rows[i]["TB_KOD_ID"]),
+						Convert.ToString(dt.Rows[i]["MAKINE_TIPI"]),
+						Convert.ToInt32(dt.Rows[i]["MAKINE_SAYISI"])
+
+						));
+				}
+				return makineTipList;
+			}
+			catch(Exception ex) 
+			{
+				return Json(new { ex.Message });
+			}
+		}
+
+		[Route("api/GetIsTalepTipEnvanter")]
+		[HttpGet]
+		public object GetIsTalepTipEnvanter([FromUri] int ID)
+		{
+			List<IsTalebiTipi> isTalebiTipList = new List<IsTalebiTipi>();
+			prms.Clear();
+			prms.Add("ISM_ID", ID);
+			try
+			{
+				query = " SELECT * FROM orjin.WEB_DASH_UDF_IS_TALEP_TIP_SAYISI() ";
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					isTalebiTipList.Add(new IsTalebiTipi
+						(
+
+						Convert.ToInt32(dt.Rows[i]["TB_KOD_ID"]),
+						Convert.ToString(dt.Rows[i]["TALEP_TIPI"]),
+						Convert.ToInt32(dt.Rows[i]["TALEP_SAYISI"])
+
+						));
+				}
+				return isTalebiTipList;
+			}
+			catch (Exception ex)
+			{
+				return Json(new { ex.Message });
+			}
+		}
+
+		[Route("api/GetIsEmriTipEnvanter")]
+		[HttpGet]
+		public object GetIsEmriTipEnvanter([FromUri] int ID)
+		{
+			List<IsEmriTipi> isEmriTipList = new List<IsEmriTipi>();
+			prms.Clear();
+			prms.Add("ISM_ID", ID);
+			try
+			{
+				query = " SELECT * FROM orjin.WEB_DASH_UDF_ISEMRI_TIP_SAYISI() ";
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					isEmriTipList.Add(new IsEmriTipi
+						(
+
+						Convert.ToInt32(dt.Rows[i]["TB_ISEMRI_TIP_ID"]),
+						Convert.ToString(dt.Rows[i]["ISEMRI_TIPI"]),
+						Convert.ToInt32(dt.Rows[i]["ISEMRI_SAYISI"])
+
+						));
+				}
+				return isEmriTipList;
+			}
+			catch (Exception ex)
+			{
+				return Json(new { ex.Message });
+			}
+		}
+
+		[Route("api/GetIsEmriDurumEnvanter")]
+		[HttpGet]
+		public object GetIsEmriDurumEnvanter([FromUri] int ID)
+		{
+			List<IsEmriDurumu> isEmriDurumList = new List<IsEmriDurumu>();
+			prms.Clear();
+			prms.Add("ISM_ID", ID);
+			try
+			{
+				query = " SELECT * FROM orjin.WEB_DASH_UDF_ISEMRI_DURUM_SAYISI() ";
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					isEmriDurumList.Add(new IsEmriDurumu
+						(
+
+						Convert.ToInt32(dt.Rows[i]["TB_KOD_ID"]),
+						Convert.ToString(dt.Rows[i]["ISEMRI_DURUMU"]),
+						Convert.ToInt32(dt.Rows[i]["ISEMRI_SAYISI"])
+
+						));
+				}
+				return isEmriDurumList;
+			}
+			catch (Exception ex)
+			{
+				return Json(new { ex.Message });
+			}
+		}
+
+		[Route("api/GetIsTalepDurumEnvanter")]
+		[HttpGet]
+		public object GetIsTalepDurumEnvanter([FromUri] int ID)
+		{
+			List<IsTalepDurumu> isTalepDurumList = new List<IsTalepDurumu>();
+			prms.Clear();
+			prms.Add("ISM_ID", ID);
+			try
+			{
+				query = " SELECT * FROM orjin.WEB_DASH_UDF_IS_TALEP_DURUM_SAYISI() ";
+				DataTable dt = klas.GetDataTable(query, prms.PARAMS);
+
+				for (int i = 0; i < dt.Rows.Count; i++)
+				{
+					isTalepDurumList.Add(new IsTalepDurumu
+						(
+
+						Convert.ToInt32(dt.Rows[i]["IST_DURUM_ID"]),
+						Convert.ToInt32(dt.Rows[i]["IS_TALEP_SAYISI"])
+
+						));
+				}
+				return isTalepDurumList;
+			}
+			catch (Exception ex)
+			{
+				return Json(new { ex.Message });
+			}
+		}
 	}
 }
--- WebApiNew/Models/WebDashboard.cs
+++ WebApiNew/Models/WebDashboard.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization;
 using WebApiNew.Models;
 
 [DataContract]
-public class WebDashboard
+public class WebDashboardCards
 {
 	[DataMember]
 	public int ACIK_IS_EMIRLERI { get; set; }
@@ -16,17 +16,8 @@ public class WebDashboard
 	public int DUSUK_STOKLU_MALZEMELER { get; set; }
 
 	[DataMember]
-	public int BEKLEYEN_MALZEME_TALEPLERI { get; set; }
-
-	[DataMember]
-	public List<MakineTipEnvanteri> MAKINE_TIP_ENVANTER { get; set; }
-
-	public WebDashboard() 
-	{
-		MAKINE_TIP_ENVANTER = new List<MakineTipEnvanteri>();
-	}
+	public int MAKINE_SAYISI { get; set; }
 }
-
 
 public class MakineTipEnvanteri 
 {
@@ -44,5 +35,77 @@ public class MakineTipEnvanteri
 		this.TB_KOD_ID = TB_KOD_ID;
 		this.MAKINE_TIPI = MAKINE_TIPI;
 		this.MAKINE_SAYISI = MAKINE_SAYISI;
+	}
+}
+
+public class IsTalebiTipi
+{
+	[DataMember]
+	public int TB_KOD_ID { get; set; }
+
+	[DataMember]
+	public string TALEP_TIPI { get; set; }
+
+	[DataMember]
+	public int TALEP_SAYISI { get; set; }
+
+	public IsTalebiTipi(int TB_KOD_ID, string TALEP_TIPI, int TALEP_SAYISI)
+	{
+		this.TB_KOD_ID = TB_KOD_ID;
+		this.TALEP_TIPI = TALEP_TIPI;
+		this.TALEP_SAYISI = TALEP_SAYISI;
+	}
+}
+
+public class IsEmriTipi
+{
+	[DataMember]
+	public int TB_ISEMRI_TIP_ID { get; set; }
+
+	[DataMember]
+	public string ISEMRI_TIPI { get; set; }
+
+	[DataMember]
+	public int ISEMRI_SAYISI { get; set; }
+
+	public IsEmriTipi(int TB_ISEMRI_TIP_ID, string ISEMRI_TIPI, int ISEMRI_SAYISI)
+	{
+		this.TB_ISEMRI_TIP_ID = TB_ISEMRI_TIP_ID;
+		this.ISEMRI_TIPI = ISEMRI_TIPI;
+		this.ISEMRI_SAYISI = ISEMRI_SAYISI;
+	}
+}
+
+public class IsEmriDurumu
+{
+	[DataMember]
+	public int TB_KOD_ID { get; set; }
+
+	[DataMember]
+	public string ISEMRI_DURUMU { get; set; }
+
+	[DataMember]
+	public int ISEMRI_SAYISI { get; set; }
+
+	public IsEmriDurumu(int TB_KOD_ID, string ISEMRI_DURUMU, int ISEMRI_SAYISI)
+	{
+		this.TB_KOD_ID = TB_KOD_ID;
+		this.ISEMRI_DURUMU = ISEMRI_DURUMU;
+		this.ISEMRI_SAYISI = ISEMRI_SAYISI;
+	}
+}
+
+public class IsTalepDurumu
+{
+	[DataMember]
+	public int IST_DURUM_ID { get; set; }
+
+	[DataMember]
+	public int IS_TALEP_SAYISI { get; set; }
+
+	public IsTalepDurumu(int IST_DURUM_ID, int IS_TALEP_SAYISI)
+	{
+		this.IST_DURUM_ID = IST_DURUM_ID;
+		this.IS_TALEP_SAYISI = IS_TALEP_SAYISI;
 	}
 }
